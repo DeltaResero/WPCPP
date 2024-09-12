af30a9f86847280db82cdcb855e9a580e05f1524
DeltaResero <3397617+DeltaResero@users.noreply.github.com>
2024-09-12 11:14:10 -0300

    Add ability to exit with controllers and improve code modularization
    Key Changes:
    
        Wii Remote Handling: Added wiiuse/wpad.h to use Wii remote input.
    
        Exit Mechanism: Added wait_for_exit() to allow the user to press 'Home' (Wii remote) or 'Start' (GameCube controller) to exit the program.
    
        Code Modularization: Broke the program into initialize_video(), calculate_pi(), and wait_for_exit() functions for clarity.
    
        Graceful Exit: Used SYS_ResetSystem(SYS_RETURNTOMENU, 0, 0) to return to the Homebrew Channel.

--- src/pi1.c
+++ src/pi1.c
@@ -4,66 +4,95 @@
 #include <malloc.h>
 #include <ogcsys.h>
 #include <gccore.h>
-
+#include <wiiuse/wpad.h>  // Wii remote input
 
 static void *xfb = NULL;
 static GXRModeObj *rmode = NULL;
 
-int main(int argc, char **argv) {
+void initialize_video() {
+    VIDEO_Init();
+    PAD_Init();  // Initialize GameCube controller
+    WPAD_Init();  // Initialize Wii remotes
 
-	VIDEO_Init();
-	PAD_Init();
-	
-	switch(VIDEO_GetCurrentTvMode()) {
-		case VI_NTSC:
-			rmode = &TVNtsc480IntDf;
-			break;
-		case VI_PAL:
-			rmode = &TVPal528IntDf;
-			break;
-		case VI_MPAL:
-			rmode = &TVMpal480IntDf;
-			break;
-		default:
-			rmode = &TVNtsc480IntDf;
-			break;
-	}
+    switch (VIDEO_GetCurrentTvMode()) {
+        case VI_NTSC:
+            rmode = &TVNtsc480IntDf;
+            break;
+        case VI_PAL:
+            rmode = &TVPal528IntDf;
+            break;
+        case VI_MPAL:
+            rmode = &TVMpal480IntDf;
+            break;
+        default:
+            rmode = &TVNtsc480IntDf;
+            break;
+    }
 
-	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
-	
-	VIDEO_Configure(rmode);
-	VIDEO_SetNextFramebuffer(xfb);
-	VIDEO_SetBlack(FALSE);
-	VIDEO_Flush();
-	VIDEO_WaitVSync();
-	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+    xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+    if (!xfb) {
+        printf("Failed to allocate framebuffer!\n");
+        exit(1);
+    }
 
-//taniver method
+    console_init(xfb, 20, 20, rmode->fbWidth, rmode->xfbHeight, rmode->fbWidth * VI_DISPLAY_PIX_SZ);
 
-    double keeptrack=1.0,pi,x,dx,y,sum=0.0,split;
+    VIDEO_Configure(rmode);
+    VIDEO_SetNextFramebuffer(xfb);
+    VIDEO_SetBlack(FALSE);
+    VIDEO_Flush();
+    VIDEO_WaitVSync();
+    if (rmode->viTVMode & VI_NON_INTERLACE) VIDEO_WaitVSync();
+}
+
+void calculate_pi() {
+    double keeptrack = 1.0, pi, x, dx, y, sum = 0.0, split;
     double a = 10000000;
-	dx=1.0;
-	split=a/100.0;
-	for(x=dx;x<=a-dx;x=x+dx)
-	{
-		y=1/((a*a)+(x*x));
-		sum=sum+(y*dx);
-		if(x>=keeptrack*split)
-		{
-			printf("\n%.0lf of 100 completed.",keeptrack);
-			keeptrack++;
-		}
-	}
-	sum=sum+((((1.0/(a*a))+(1/(2*a*a)))/2.0)*dx);
-	pi=4.0*sum*a;
+    dx = 1.0;
+    split = a / 100.0;
+
+    for (x = dx; x <= a - dx; x += dx) {
+        y = 1 / ((a * a) + (x * x));
+        sum += (y * dx);
+
+        if (x >= keeptrack * split) {
+            printf("\n%.0lf of 100 completed.", keeptrack);
+            keeptrack++;
+        }
+    }
+
+    sum += ((((1.0 / (a * a)) + (1 / (2 * a * a))) / 2.0) * dx);
+    pi = 4.0 * sum * a;
     printf("\nPi Calculation Complete!\n");
-	printf("\nPi=%.50lf\n",pi);
-	return(0);
+    printf("\nPi = %.50lf\n", pi);
+}
 
-	while(1) {
+void wait_for_exit() {
+    printf("\nPress 'Home' on Wii Remote or 'Start' on GameCube controller to exit.\n");
 
-		VIDEO_WaitVSync();
-	}
+    while (1) {
+        PAD_ScanPads();
+        WPAD_ScanPads();
 
+        u32 gc_pressed = PAD_ButtonsDown(0);  // GameCube controller
+        u32 wii_pressed = WPAD_ButtonsDown(0);  // Wii remote
+
+        if (gc_pressed & PAD_BUTTON_START || wii_pressed & WPAD_BUTTON_HOME) {
+            printf("\nExiting to Homebrew Channel...\n");
+            VIDEO_WaitVSync();
+            SYS_ResetSystem(SYS_RETURNTOMENU, 0, 0);  // Return to Homebrew Channel
+        }
+
+        VIDEO_WaitVSync();
+    }
+}
+
+int main(int argc, char **argv) {
+    initialize_video();
+
+    calculate_pi();
+
+    wait_for_exit();  // Wait for the user to press the exit button
+
+    return 0;
 }
